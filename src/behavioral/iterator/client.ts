b7281750c29c3943a744fb62a11e9699e2c4e3b8
bogdan9876 <bogdankryveiko@gmail.com>
2025-04-25 16:46:29 +0300

    iterator

---
+++ src/behavioral/iterator/client.ts
@@ -0,0 +1,20 @@
+
+class NumberWheel {
+
+    index: number
+
+    constructor() {
+        this.index = 0
+    }
+
+    next() {
+        this.index = this.index + 1
+        return (this.index * 2) % 11
+    }
+}
+
+const NUMBERWHEEL = new NumberWheel()
+
+for (let i = 0; i < 22; i++) {
+    process.stdout.write(NUMBERWHEEL.next() + ' ')
+}
---
+++ src/behavioral/iterator/concept/iterator-concept.ts
@@ -0,0 +1,51 @@
+interface IIterator {
+    next(): IAggregate
+
+    hasNext(): boolean
+}
+
+class IteratorConcept implements IIterator {
+    index: number
+    aggregates: IAggregate[]
+
+    constructor(aggregates: IAggregate[]) {
+        this.index = 0
+        this.aggregates = aggregates
+    }
+
+    next() {
+        if (this.index < this.aggregates.length) {
+            const aggregate = this.aggregates[this.index]
+            this.index += 1
+            return aggregate
+        }
+        throw new Error('At End of Iterator')
+    }
+
+    hasNext() {
+        return this.index < this.aggregates.length
+    }
+}
+
+interface IAggregate {
+    method(): void
+}
+
+class Aggregate implements IAggregate {
+    method(): void {
+        console.log('This method has been invoked')
+    }
+}
+
+const AGGREGATES = [
+    new Aggregate(),
+    new Aggregate(),
+    new Aggregate(),
+    new Aggregate(),
+]
+
+const ITERABLE = new IteratorConcept(AGGREGATES)
+
+while (ITERABLE.hasNext()) {
+    ITERABLE.next().method()
+}
